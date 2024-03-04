cf36f65cb3c63e0dd09b56382e05291ffd779a0c
Natalia Andrzejewska <n.andrzejewska27@gmail.com>
2024-03-04 23:39:54 +0100

    pliki_zadania

---
+++ lab2_zadanie8/lab2_zadanie8/lab2_zadanie8.cpp
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+#include <limits.h>
+#include <float.h>
+#include <math.h>
+
+int main (int argc, char* argv[])
+{
+	double x = 3.14;
+	double y = 12.56;
+	double z = 7;
+	double k = 2;
+	double m = 4;
+
+	double w1 = (pow((x / (y * z)), (1. / 3)) * log((pow(x, 2.)+pow(y, 2.))));
+	double w2 = (sin(k * (x / 2.)) * cos(m * y) + y * exp(2*x-1));
+	double w3 = ((fabs(x / ((2 * pow(y, 2.)) + 1))) + (pow((y / ((pow(z, 2.))) + 3), (1. / 2)))+(5 * (pow((y + z), 3.))));
+	double w4 = (((x / (y * z)) * (pow((z + 1), 1. / 3))) + (pow(((pow(z, 2.)) + (pow(x, 2.)) + 1), 1./k)) - (fabs(y)));
+	double w5 = ((1 / (pow((pow(x, 2.)) + (pow(y, 2.)) + (pow(k, 2.)), 1. / 2))) + ((1. / x) * sin(k * y)));
+
+	printf("%.10lf\r\n", w1);
+	printf("%.10lf\r\n", w2);
+	printf("%.10lf\r\n", w3);
+	printf("%.10lf\r\n", w4);
+	printf("%.10lf\r\n", w5);
+}
---
+++ lab2_zadanie9/lab2_zadanie9/lab2_zadanie9.cpp
@@ -0,0 +1,55 @@
+﻿
+
+#include <math.h>
+#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+int main()
+{
+	double a = 0.0, b = 0.0, c = 0.0;
+	int ret = -1;
+	
+	printf("Program obliczenia rzeczywistych pierwiastkow rownania kwadratowego\n");
+	printf("-------------------- a*x^2+b*x+c = 0 ------------------------------\n\n");
+	printf("Podaj a,b,c\n");
+	ret = scanf("%le%le%le", &a, &b, &c);
+	if (ret != 3)
+	{
+		printf("Blad przy wczytaniu\n");
+		printf("Wczytane: a = %e b = %e c = %e\n", a, b, c);
+		
+		;
+	}
+	
+	if (a == 0.0) 
+	{
+		printf("To nie jest rownanie kwadratowe: a = 0.\n");
+		
+		;
+	}
+	
+	double D = -1.0; 
+	D = b * b - 4.0 * a * c;
+	if (D < 0.0)
+	{
+		double r1, r2, u1, u2;
+		r1 = ( ( - b) / (2 * a));
+		u1 = (pow((fabs(pow(b / (2.0*a),2) - (c / a))),1. / 2));
+		r2 = (( - b) / (2.0*a));
+		u2 = (- (pow((fabs(pow(b / (2.0 * a),2) - (c / a))), 1. / 2)));
+		printf("Pierwiastki:x1=%e+%e*i x2=%e+%e*i", r1, u1, r2 ,u2);
+	}
+	else {
+		double p1, p2, x1, x2;
+		p1 = -b / (2.0 * a);
+		p2 = sqrt(D) / (2.0 * a);
+		x1 = p1 + p2;
+		x2 = p1 - p2;
+		printf("Rownanie kwadratowe: %lf *X*X + %lf *X + %lf = 0\n\n", a, b, c);
+		printf("Pierwiastki: x1 = %e x2 = %e\n", x1, x2);
+		printf("---------------------------------------------------------");
+	}
+	return 0;
+}
+
+
